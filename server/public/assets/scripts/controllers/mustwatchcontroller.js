4bb7b951c054b8f2c1ba3e75e57f7c0756ac24f7
Zeinab Hassan <zeinab.hassan101@gmail.com>
2017-06-12 13:15:09 -0500

    modified some styling and functionality

--- server/public/assets/scripts/controllers/mustwatchcontroller.js
+++ server/public/assets/scripts/controllers/mustwatchcontroller.js
@@ -24,7 +24,22 @@
 
    vm.deleteAllMustWatch = function(id){
       mustwatchservice.deleteWatch(id).then(function(){
-      vm.getAllMustWatch();  
+        swal({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  type: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then(function () {
+  swal(
+    'Deleted!',
+    'Your file has been deleted.',
+    'success'
+  );
+});
+      vm.getAllMustWatch();
       });
    };
  }); //end controller
--- server/public/assets/scripts/controllers/upcomingcontroller.js
+++ server/public/assets/scripts/controllers/upcomingcontroller.js
@@ -22,6 +22,21 @@ vm.getAllUpcoming = function(){
 }; //end getAllUpcoming
 
 vm.deleteAllUpcoming = function(id){
+  swal({
+title: 'Are you sure?',
+text: "You won't be able to revert this!",
+type: 'warning',
+showCancelButton: true,
+confirmButtonColor: '#3085d6',
+cancelButtonColor: '#d33',
+confirmButtonText: 'Yes, delete it!'
+}).then(function () {
+swal(
+'Deleted!',
+'Your file has been deleted.',
+'success'
+);
+});
   trackupcomingService.deleteUpcoming(id).then(function(response){
     vm.getAllUpcoming();
   });
